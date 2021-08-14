acca60944ce44d1a604bdbacf65b8c0d41b2f0cc
ChunGeun-Yu <chungeun.yu@gmail.com>
2021-08-14 20:57:57 +0900

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -12,4 +12,28 @@ public class Controller {
 		System.out.println("get...");
 		return "{\"id\": \"1234567890\"}";			
 	}
+	
+	@GetMapping("/api1")
+	public String get() {
+		System.out.println("get...");
+		return "{\"id\": \"api1\"}";			
+	}
+	
+	@GetMapping("/api2")
+	public String get() {
+		System.out.println("get...");
+		return "{\"id\": \"api2\"}";			
+	}
+	
+	@GetMapping("/api3")
+	public String get() {
+		System.out.println("get...");
+		return "{\"id\": \"get_api3\"}";			
+	}
+	
+	@PostMapping("/api3")
+	public String get() {
+		System.out.println("post...");
+		return "{\"id\": \"post_api3\"}";			
+	}
 }
