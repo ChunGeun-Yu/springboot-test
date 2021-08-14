811c6385dc79548fc8d7ab52b0774058142520a4
ChunGeun-Yu <chungeun.yu@gmail.com>
2021-08-14 22:28:45 +0900

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
